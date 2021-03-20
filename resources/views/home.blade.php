f651011a0593d163f2b1e8a0e633ec39bc807b47
Ilyas Hidayat Rusdy <saylihidayatrusdy@gmail.com>
2021-03-20 13:37:10 +0700

    first

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('title','Home')
+@section('judul','Home')
+@section('content')
+<div class="flex flex-wrap">
+    <div class="w-full p-6">
+        <h1><b>Teknik Informatika</b></h1>
+        <hr>
+        <h2>Ilyas Hidayat Rusdy</h2>
+        <h2>1915101021</h2>
+        <h2>Ilmu Komputer 4A</h2>
+    </div>
+</div>
+@endsection
